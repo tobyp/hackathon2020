25871e2200b81a822b3699b662d5164ebcae39c5
Flakebi <flakebi@t-online.de>
2020-12-26 16:37:17 +0100

    Duplicate material for each cell

--- cells/cell.gd
+++ cells/cell.gd
@@ -1,22 +1,19 @@
 extends Node2D
 
-##############################
-# TODO own Resources here (?)
-##############################
-
 func _input(event):
 	# Mouse in viewport coordinates.
 	if event is InputEventMouseButton:
-		# More biomass
-		var cur = $Gfx.material.get_shader_param("percentage")
-		if event.button_index == BUTTON_LEFT:
-			$Gfx.material.set_shader_param("percentage", min(1.0, cur + 0.2))
-		else:
-			$Gfx.material.set_shader_param("percentage", max(0.0, cur - 0.2))
+		if event.is_pressed():
+			# More biomass
+			var cur = $Gfx.material.get_shader_param("percentage")
+			if event.button_index == BUTTON_LEFT:
+				$Gfx.material.set_shader_param("percentage", min(1.0, cur + 0.2))
+			elif  event.button_index == BUTTON_RIGHT:
+				$Gfx.material.set_shader_param("percentage", max(0.0, cur - 0.2))
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Gfx.set_material($Gfx.get_material().duplicate())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
