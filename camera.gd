9fd5e0529f6de331345f542782d72d856d42fd38
tobyp <tobyp@tobyp.net>
2020-12-26 21:25:46 +0100

    Implement window-edge panning in full-screen

--- camera.gd
+++ camera.gd
@@ -4,11 +4,14 @@ extends Camera2D
 var dragging = false
 var startPos
 # Keyboard
-var moveVec = Vector2(0, 0)
+var moveVec = Vector2.ZERO
+# Edge panning
+var edgePanVec = Vector2.ZERO
 
-const CAM_MIN = 0.5
-const CAM_MAX = 5.0
-const KEY_MOVE = 20.0
+const ZOOM_MIN = 1.0
+const ZOOM_MAX = 5.0
+const KEY_PAN_SPEED = 1970.0 / 2.0  # 2.0 is default zoom
+const EDGE_PAN_SPEED = KEY_PAN_SPEED
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -18,25 +21,36 @@ func _input(event):
 				startPos = event.position
 		elif event.button_index == BUTTON_WHEEL_UP:
 			zoom /= 1.2
-			if zoom.x < CAM_MIN:
-				zoom = Vector2(CAM_MIN, CAM_MIN)
+			if zoom.x < ZOOM_MIN:
+				zoom = Vector2(ZOOM_MIN, ZOOM_MIN)
 		elif event.button_index == BUTTON_WHEEL_DOWN:
 			zoom *= 1.2
-			if zoom.x > CAM_MAX:
-				zoom = Vector2(CAM_MAX, CAM_MAX)
-	elif event is InputEventMouseMotion and dragging:
-		position += (startPos - event.position) * zoom
-		startPos = event.position
+			if zoom.x > ZOOM_MAX:
+				zoom = Vector2(ZOOM_MAX, ZOOM_MAX)
+	elif event is InputEventMouseMotion:
+		if dragging:
+			position += (startPos - event.position) * zoom
+			startPos = event.position
+		edgePanVec = Vector2.ZERO
+		if OS.window_fullscreen:
+			if event.position.x == 0:
+				 edgePanVec.x = -EDGE_PAN_SPEED
+			elif event.position.x >= OS.window_size.x - 1:
+				edgePanVec.x = EDGE_PAN_SPEED
+			if event.position.y == 0:
+				 edgePanVec.y = -EDGE_PAN_SPEED
+			elif event.position.y >= OS.window_size.y - 1:
+				edgePanVec.y = EDGE_PAN_SPEED
 	elif event is InputEventKey:
 		if event.is_pressed():
 			if event.get_scancode() == KEY_W:
-				moveVec.y = -KEY_MOVE
+				moveVec.y = -KEY_PAN_SPEED
 			elif event.get_scancode() == KEY_A:
-				moveVec.x = -KEY_MOVE
+				moveVec.x = -KEY_PAN_SPEED
 			elif event.get_scancode() == KEY_S:
-				moveVec.y = KEY_MOVE
+				moveVec.y = KEY_PAN_SPEED
 			elif event.get_scancode() == KEY_D:
-				moveVec.x = KEY_MOVE
+				moveVec.x = KEY_PAN_SPEED
 		else:
 			if event.get_scancode() == KEY_W or event.get_scancode() == KEY_S:
 				moveVec.y = 0
@@ -51,4 +65,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# TODO use delta here
-	position += moveVec * zoom
+	position += moveVec * zoom * delta
+	position += edgePanVec * zoom * delta
