9944cd8c3df510d520d2f5f89700578a7b2d53e5
felixk101 <felixk101@gmail.com>
2020-12-26 21:53:44 +0100

    add felix's missing ui folder

---
+++ ui/MyColorRect.gd
@@ -0,0 +1,20 @@
+extends ColorRect
+
+
+
+var states = ["closed", "openLeft", "openRight", "openBoth"]
+var state_index = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			state_index = (state_index + 1) % 4
+			color = Color(1,state_index/4.0,1)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
