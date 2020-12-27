dc872d95d70c7ab91bc0fea2b6009b4fdd3cc514
felixk101 <felixk101@gmail.com>
2020-12-27 17:51:26 +0100

    fix create_tunnels in main.gd

--- main.gd
+++ main.gd
@@ -15,12 +15,14 @@ func _ready():
 
 	var edges = $HexGrid.get_undirected_node_connections()
 	for connection in edges:
-		var start_pos = $HexGrid.create_cell(connection[0].x, connection[0].y).position
-		var end_pos = $HexGrid.create_cell(connection[1].x, connection[1].y).position
+		var start_cell = $HexGrid.create_cell(connection[0].x, connection[0].y)
+		var start_pos = start_cell.position
+		var end_cell = $HexGrid.create_cell(connection[1].x, connection[1].y)
+		var end_pos = end_cell.position
 		var middle = 0.5 * (start_pos + end_pos)
 		var the_angle = (start_pos.direction_to(end_pos)).angle()
 		# print("the middle of "+str(start_pos)+" and "+str(end_pos)+" is "+str(middle)+ ", angl "+str(the_angle))
-		$HexGrid.draw_tunnels(middle, the_angle)
+		$HexGrid.create_tunnels(middle, the_angle, start_cell, end_cell)
 
 func debug_populate():
 	var cell1 = $HexGrid.create_cell(0, 0)  # 1-indexed becase that matches get_index() on the cells, in this case
